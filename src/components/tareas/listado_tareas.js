3c4d27dcdc21015916e0f32e629d516a8127e02a
attiliogiglio <attilio.g93@gmail.com>
2020-09-28 17:29:08 -0300

    gui tareas

--- src/components/tareas/listado_tareas.js
+++ src/components/tareas/listado_tareas.js
@@ -24,6 +24,12 @@ const ListadoTareas = () => {
                         ))
                 }
             </ul>
+            <button
+                type='button'
+                className='btn btn-eliminar'
+            >
+                Eliminar Proyecto &times;
+            </button>
         </Fragment>
     )
 }
--- src/components/tareas/tarea.js
+++ src/components/tareas/tarea.js
@@ -1,9 +1,44 @@
 import React from 'react'
 
-const Tarea = ({tareas}) => {
+const Tarea = ({ tarea }) => {
     return (
-        <li className='tarea so'>
+        <li className="tarea sombra">
+            <p>{tarea.nombre} </p>
 
+            <div className="estado">
+                {tarea.estado 
+                ?  
+                    (
+                        <button
+                            type="button"
+                            className="completo"
+                            // onClick={() => cambiarEstado(tarea)}
+                        >Completo</button>
+                    )
+                : 
+                    (
+                        <button
+                            type="button"
+                            className="incompleto"
+                            // onClick={() => cambiarEstado(tarea)}
+                        >Incompleto</button>
+                    )
+                }
+            </div>
+
+            <div className="acciones">
+                <button 
+                    type="button"
+                    className="btn btn-primario"
+                    // onClick={() => seleccionarTarea(tarea) }
+                >Editar</button>
+
+                <button
+                    type="button"
+                    className="btn btn-secundario"
+                    // onClick={() => tareaEliminar(tarea._id)}
+                >Eliminar</button>
+            </div>
         </li>
     )
 }
