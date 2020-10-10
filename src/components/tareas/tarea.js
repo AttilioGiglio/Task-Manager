6e67c432531564620dcc73680980155af41746e4
attiliogiglio <attilio.g93@gmail.com>
2020-10-10 12:19:42 -0300

    create delete function to btn from each task

--- src/components/tareas/tarea.js
+++ src/components/tareas/tarea.js
@@ -1,42 +1,62 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import TareaContext from '../../context/tareas/tarea_context';
+import ProyectoContext from '../..//context/proyectos/proyecto_context';
 
 const Tarea = ({ tarea }) => {
+
+    // Extraer si un proyecto esta activo
+    const proyectoContext = useContext(ProyectoContext);
+    const { proyecto } = proyectoContext;
+
+    // obtener la función del context tarea
+    const tareaContext = useContext(TareaContext);
+    const { eliminarTarea, obtenerTareas } = tareaContext;
+
+    // Extraer el proyecto
+    const [proyectoActual] = proyecto;
+
+    // Funcion que se ejecuta cuando el usuario presiona el btn de eliminar tarea
+    const tareaEliminar = id => {
+        eliminarTarea(id);
+        obtenerTareas(proyectoActual.id);
+    }
+
     return (
         <li className="tarea sombra">
             <p>{tarea.nombre} </p>
 
             <div className="estado">
-                {tarea.estado 
-                ?  
+                {tarea.estado
+                    ?
                     (
                         <button
                             type="button"
                             className="completo"
-                            // onClick={() => cambiarEstado(tarea)}
+                        // onClick={() => cambiarEstado(tarea)}
                         >Completo</button>
                     )
-                : 
+                    :
                     (
                         <button
                             type="button"
                             className="incompleto"
-                            // onClick={() => cambiarEstado(tarea)}
+                        // onClick={() => cambiarEstado(tarea)}
                         >Incompleto</button>
                     )
                 }
             </div>
 
             <div className="acciones">
-                <button 
+                <button
                     type="button"
                     className="btn btn-primario"
-                    // onClick={() => seleccionarTarea(tarea) }
+                // onClick={() => seleccionarTarea(tarea) }
                 >Editar</button>
 
                 <button
                     type="button"
                     className="btn btn-secundario"
-                    // onClick={() => tareaEliminar(tarea._id)}
+                    onClick={() => tareaEliminar(tarea.id)}
                 >Eliminar</button>
             </div>
         </li>
--- src/context/tareas/tarea_state.js
+++ src/context/tareas/tarea_state.js
@@ -1,24 +1,24 @@
 import React, { useReducer } from 'react';
 import TareaContext from './tarea_context';
 import TareaReducer from './tarea_reducer';
-import { TAREAS_PROYECTO, AGREGAR_TAREA, VALIDAR_TAREA } from '../../types/constants';
+import { TAREAS_PROYECTO, AGREGAR_TAREA, VALIDAR_TAREA, ELIMINAR_TAREA } from '../../types/constants';
 
 const TareaState = props => {
     const initialState = {
         tareas: 
         [
-            {nombre: 'elegir Plataforma', estado: true, proyectId: 1},
-            {nombre: 'elegir Colores', estado: false, proyectId: 2},
-            {nombre: 'elegir Plataforma de pago', estado: true, proyectId: 3},
-            {nombre: 'elegir Hosting', estado: false, proyectId: 1},
-            {nombre: 'elegir Colores', estado: true, proyectId: 2},
-            {nombre: 'elegir Plataforma de pago', estado: false, proyectId: 3},
-            {nombre: 'elegir Hosting', estado: true, proyectId: 4},
-            {nombre: 'elegir Colores', estado: false, proyectId: 1},
-            {nombre: 'elegir Plataforma', estado: true, proyectId: 2},
-            {nombre: 'elegir Hosting', estado: false, proyectId: 3},
-            {nombre: 'elegir Colores', estado: true, proyectId: 4},
-            {nombre: 'elegir Plataforma de pago', estado: false, proyectId: 1},
+            {id:1, nombre: 'elegir Plataforma', estado: true, proyectId: 1},
+            {id:2, nombre: 'elegir Colores', estado: false, proyectId: 2},
+            {id:3, nombre: 'elegir Plataforma de pago', estado: true, proyectId: 3},
+            {id:4, nombre: 'elegir Hosting', estado: false, proyectId: 1},
+            {id:5, nombre: 'elegir Colores', estado: true, proyectId: 2},
+            {id:6, nombre: 'elegir Plataforma de pago', estado: false, proyectId: 3},
+            {id:7, nombre: 'elegir Hosting', estado: true, proyectId: 4},
+            {id:8, nombre: 'elegir Colores', estado: false, proyectId: 1},
+            {id:9, nombre: 'elegir Plataforma', estado: true, proyectId: 2},
+            {id:10, nombre: 'elegir Hosting', estado: false, proyectId: 3},
+            {id:11, nombre: 'elegir Colores', estado: true, proyectId: 4},
+            {id:12, nombre: 'elegir Plataforma de pago', estado: false, proyectId: 1},
         ],
         tareasproyecto: null,
         errortarea: false
@@ -29,10 +29,10 @@ const TareaState = props => {
     // crear las funciones 
     
     // obtener las tareas de un proyecto
-    const obtenerTareas = proyectId => {
+    const obtenerTareas = proyectoId => {
         dispatch({
             type: TAREAS_PROYECTO,
-            payload: proyectId
+            payload: proyectoId
         })
     }
 
@@ -49,6 +49,13 @@ const TareaState = props => {
         })
     }
 
+    const eliminarTarea = id => {
+        dispatch({
+            type: ELIMINAR_TAREA,
+            payload: id
+        })
+    }
+
     return(
         <TareaContext.Provider value={{
             tareas: state.tareas,
@@ -56,7 +63,8 @@ const TareaState = props => {
             errortarea: state.errortarea,
             obtenerTareas,
             agregarTarea,
-            validarTarea
+            validarTarea,
+            eliminarTarea
             }}>
             {props.children}
         </TareaContext.Provider>
