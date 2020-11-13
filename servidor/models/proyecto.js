0fac239dfbc16441ce2368ec331bd1032c822234
attiliogiglio <attilio.g93@gmail.com>
2020-11-13 19:37:06 -0300

    backend: servidor: new model proyecto

---
+++ servidor/models/proyecto.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const ProyectoSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario'
+
+    },
+    create: {
+        type:Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Proyecto', ProyectoSchema)
